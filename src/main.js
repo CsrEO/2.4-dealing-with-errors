fd55dbecfded10ad9ab0256390d51075791071cc
Cesar Esquivel <czr.esquivel@gmail.com>
2022-03-31 20:46:31 -0500

    this is a solution for 2.4

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 const myAge = 21;
-let drivingAge = 12;
+const hasLicense = false;
+const drivingAge = 18;
 
-if(myAge>=drivingAge){
+if(myAge>=drivingAge && hasLicense){
     console.log("you can drive");
 } else{
     console.log("you can not drive");
